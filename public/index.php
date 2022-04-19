bd7e0098b88ebf842dcb0eb10b8b973bb595e57b
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-04-19 23:45:27 +0500

    Rocks Game Bot

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Rocks Gami Bot - Telegram Bot (@RocksGameBot)
+ *
+ * (c) 2022 rocks
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Bot\BotCore;
+
+/**
+ * Handle webhook request only when it's a POST request
+ */
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once __DIR__ . ' /../vendor/autoload.php';
+
+    try {
+        $app = new BotCore();
+        $app->run(true);
+    } catch (\Throwable $e) {
+        // Prevent Telegram from retrying
+    }
+} else {
+    header("Location: https://github.com/jankarikiduniya/RocksGameBot");    // Redirect non-POST requests to Github repository
+}
