20f9effc5707e59a166d644ea54e06b0177b6b0e
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-04-20 00:05:58 +0500

    Rocks Game Bot

--- config.example.php
+++ config.example.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Rocks Gami Bot - Telegram Bot (@RocksGameBot)
+ * Inline Games - Telegram Bot (@inlinegamesbot)
  *
- * (c) 2022 rocks
+ * (c) 2016-2021 Jack'lul <jacklulcat@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
