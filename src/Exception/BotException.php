31f27a10b07509132dedef9874d77bc39630e331
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-04-19 23:35:26 +0500

    Rocks Game Bot

---
+++ src/Exception/BotException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Rocks Gami Bot - Telegram Bot (@RocksGameBot)
+ *
+ * (c) 2022 rocks
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bot\Exception;
+
+use Exception;
+
+/**
+ * Exception class used for bot logic related exception handling
+ */
+class BotException extends Exception
+{
+}
