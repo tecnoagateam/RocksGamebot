5a4c0b0fd121e9164ab31c63267fe8642b6a053c
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2022-04-20 00:10:35 +0500

    Network Added

--- src/Command/User/StartCommand.php
+++ src/Command/User/StartCommand.php
@@ -58,7 +58,7 @@ class StartCommand extends UserCommand
 
         $text = Emoji::wavingHand() . ' ';
         $text .= '<b>' . __('Hi!') . '</b>' . PHP_EOL;
-        $text .= __('To begin or join @Alexa_Help bot owner @Dr_Asad_Ali contact me to get repo, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
+        $text .= __('To begin or join @Alexa_Help and @Shayri_Music_Lovers bot owner @Dr_Asad_Ali contact me to get repo, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
 
         $data = [
             'chat_id'                  => $chat_id,
