aee5be1d9bd4652796bb78e7013a62019ddbeb92
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-08 09:36:25 +0400

    Update StartCommand.php

--- src/Command/User/StartCommand.php
+++ src/Command/User/StartCommand.php
@@ -58,7 +58,7 @@ class StartCommand extends UserCommand
 
         $text = Emoji::wavingHand() . ' ';
         $text .= '<b>' . __('Hi!') . '</b>' . PHP_EOL;
-        $text .= __('To begin or join @Alexa_Help and @Shayri_Music_Lovers bot owner @Dr_Asad_Ali contact me to get repo, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
+        $text .= __('salam, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Oyna') . '\'</b>']);
 
         $data = [
             'chat_id'                  => $chat_id,
@@ -69,7 +69,7 @@ class StartCommand extends UserCommand
                 [
                     new InlineKeyboardButton(
                         [
-                            'text'                => __('Play') . ' ' . Emoji::gameDie(),
+                            'text'                => __('Oyna') . ' ' . Emoji::gameDie(),
                             'switch_inline_query' => Emoji::gameDie(),
                         ]
                     ),
